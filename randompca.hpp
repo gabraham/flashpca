8139f3f8a5e385878d74c426b7c72bd7c69a9eba
Gad Abraham <abrahamg@unimelb.edu.au>
2014-05-04 13:37:49 +1000

    Removed lambda penalty, replaced with lambda1 and lambda2

--- randompca.hpp
+++ randompca.hpp
@@ -39,8 +39,8 @@ class RandomPCA {
 	    int kernel, double sigma, bool rbf_center,
 	    unsigned int rbf_sample, bool save_kernel,
 	    bool do_orth);
-      void cca(MatrixXd &X, MatrixXd &Y, double lambda, long seed);
-//      void cca_qr(MatrixXd &X, MatrixXd &Y, double lambda, long seed);
+      void cca(MatrixXd &X, MatrixXd &Y, double lambda1, double lambda2,
+	    long seed);
       void scca(MatrixXd &X, MatrixXd &Y, double lambda1, double lambda2,
 	    long seed, unsigned int dim);
       void zca_whiten(bool transpose);
